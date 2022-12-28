37eeb0d81fe63f750d2c2f50a22f2db051576e15
Sandiega <sandiegasutoko12@gmail.com>
2022-12-28 11:36:21 +0700

    ganti tipe rating stars jadi integer

--- FashRent/database/migrations/2022_12_14_115739_product_feedback.php
+++ FashRent/database/migrations/2022_12_14_115739_product_feedback.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->id('feedback_id');
             $table->foreignid('renter_id')->references('renter_id')->on('renter')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignid('product_id')->references('product_id')->on('product')->onUpdate('cascade')->onDelete('cascade');
-            $table->string('rating_stars');
+            $table->integer('rating_stars');
             $table->timestamp('rating_date');
             $table->string('rating_description');
             $table->string('rating_photo');
